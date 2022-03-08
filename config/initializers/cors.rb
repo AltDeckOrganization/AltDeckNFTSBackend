b638bd21f7371775ca0ffa0ddeb707fb022e2298
pwilson77 <p.o.yirenkyi@gmail.com>
2022-03-08 08:57:10 +0000

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000'
+      if Rails.env.development?
+        origins 'http://localhost:3000'
+      else
+        origins 'https://aqueous-dawn-46734.herokuapp.com'
+      end
       resource '*', headers: :any, methods: [:get, :post, :put, :delete]
     end
 end
