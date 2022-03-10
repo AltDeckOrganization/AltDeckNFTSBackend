62b4eff5d8dada44678cb098eb50e96fd35928cd
pwilson77 <p.o.yirenkyi@gmail.com>
2022-03-10 20:37:48 +0000

    fix for cors.config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       if Rails.env.development?
         origins 'http://localhost:3000'
       else
-        origins 'https://quirky-lewin-72018a.netlify.app', 'https://altdeck.io', 'https://www.altdeck.io', 'https://focused-wright-78670e.netlify.app/'
+        origins 'https://quirky-lewin-72018a.netlify.app', 'https://altdeck.io', 'https://www.altdeck.io', 'https://focused-wright-78670e.netlify.app'
       end
       resource '*', headers: :any, methods: [:get, :post, :put, :delete]
     end
