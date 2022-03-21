a5b06b5fc6f2b24417d43dda3b637f8c045db1de
Joboa <joboa2015@gmail.com>
2022-03-21 11:52:52 +0000

    add validations to lauchpad_statistics model

--- app/models/launchpad_statistic.rb
+++ app/models/launchpad_statistic.rb
@@ -1,3 +1,10 @@
 class LaunchpadStatistic < ApplicationRecord
+  # include ActionView::Helpers::NumberHelper
+
   belongs_to :launch
+
+  validates :whitelist_mint_settings, presence: true
+  validates :mint_price, presence: true
+  validates :mint_currency, presence: true
+  # number_with_precision(:mint_currency, precision: 4)
 end
