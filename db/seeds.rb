8c028af8a9b656ae25d401eeaf22638613020d8f
Joboa <joboa2015@gmail.com>
2022-03-21 13:52:43 +0000

    add test data
    update launchpad_statistics controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Launches
+launch = Launch.create(name: "3D War", profile_image_path: "/a/b/c",
+  form_data: 'NFT is here', candymachine_id: "abcd", active: true,)
+  
+launch1 = Launch.create(name: "3D Games", profile_image_path: "/d/e/f",
+  form_data: 'Blockchain', candymachine_id: "efgh", active: false,)
+    
+launch2 = Launch.create(name: "X-Men Wallpaper", profile_image_path: "/g/h/i",
+  form_data: 'Get a copy now', candymachine_id: "ijkl", active: true,)
+      
+
+# Launchpad statistics
+stats = LaunchpadStatistic.create(launch_id: launch, whitelist_mint_settings: "[a, b, c, d]",
+  mint_price: 34, mint_currency: 345.50)
+
+stats1 = LaunchpadStatistic.create(launch_id: launch1, whitelist_mint_settings: "[e, f, g, h]",
+  mint_price: 44, mint_currency: 647.50555)
+
+stats2 = LaunchpadStatistic.create(launch_id: launch2, whitelist_mint_settings: "[i, j, k, l]",
+    mint_price: 76, mint_currency: 896.4345)
