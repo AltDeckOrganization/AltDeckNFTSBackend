0ceaa26d55cf5223afc53f5628ffdddda9d85129
pwilson77 <p.o.yirenkyi@gmail.com>
2022-04-14 16:37:27 +0000

    temporarily switched cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       if Rails.env.development?
         origins 'http://localhost:3000', 'http://localhost:3001'
       else
-        origins 'https://quirky-lewin-72018a.netlify.app', 'https://altdeck.io', 'https://www.altdeck.io', 'https://focused-wright-78670e.netlify.app'
+        origins 'https://quirky-lewin-72018a.netlify.app', 'https://altdeck.io', 'https://www.altdeck.io', 'https://focused-wright-78670e.netlify.app', 'http://localhost:3000'
       end
       resource '*', headers: :any, methods: [:get, :post, :put, :delete]
     end
